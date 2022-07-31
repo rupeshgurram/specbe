aa8d2c607d045f3aa681ac6855ba9fb7ce7f6a0c
rupeshgurram <110326472+rupeshgurram@users.noreply.github.com>
2022-07-31 22:47:14 +0530

    change1

--- specbee/src/Form/SettingsForm.php
+++ specbee/src/Form/SettingsForm.php
@@ -39,36 +39,36 @@ class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('specbee.settings');
 	
-	$dropdowns = array(
-	  "0"=>'select List',
-      "america/chicago" => 'America/Chicago',
-	  "america/new_York" => 'America/New_York',
-	  "asia/tokyo" => 'Asia/Tokyo',
-	  "asia/dubai" => 'Asia/Dubai',
-	  "asia/kolkata" => 'Asia/Kolkata',
-	  "europe/amsterdam" => 'Europe/Amsterdam',
-	  "europe/oslo" => 'Europe/Oslo',
-	  "europe/london" => 'Europe/London',
-	);
+    $dropdowns = array(
+      "0"=>'select List',
+        "america/chicago" => 'America/Chicago',
+	"america/new_York" => 'America/New_York',
+	"asia/tokyo" => 'Asia/Tokyo',
+	"asia/dubai" => 'Asia/Dubai',
+	"asia/kolkata" => 'Asia/Kolkata',
+	"europe/amsterdam" => 'Europe/Amsterdam',
+	"europe/oslo" => 'Europe/Oslo',
+	"europe/london" => 'Europe/London',
+    );
 
     $form['Country'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Mention your country'),
-	  '#required => TRUE'
+      '#required => TRUE'
       '#default_value' => $config->get('Country'),
     );
 	
-	$form['City'] = array(
+    $form['City'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Mention your City'),
-	  '#required => TRUE'
+      '#required => TRUE'
       '#default_value' => $config->get('City'),
     );
 	
-	$form['Timezone'] = array(
+    $form['Timezone'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Mention your Timezone'),
-	  '#required => TRUE'	  
+      '#required => TRUE'	  
       '#default_value' => $dropdowns,
     );
 
@@ -81,8 +81,8 @@ class SettingsForm extends ConfigFormBase {
 
     $this->config('specbee.settings')
       ->set('Country', $form_state->getValue('Country'))
-	  ->set('City', $form_state->getValue('City'))
-	  ->set('Timezone', $form_state->getValue('Timezone'))
+      ->set('City', $form_state->getValue('City'))
+      ->set('Timezone', $form_state->getValue('Timezone'))
       ->save();
   }
 
